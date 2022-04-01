99e7bc1765ebaa2742437a0e8b868e65a371fc2d
bussury <bussury.omary@gmail.com>
2022-04-01 15:05:44 +0300

    p

--- entry/server.js
+++
@@ -1,9 +0,0 @@
-import { createApp}  from '../resources/client.js'
-import { createRenderFunction } from 'fastify-vite-vue/server'
-
-import routes from '../routes/web/web.js'
-
-export default{
-    routes,
-    render: createRenderFunction(createApp),
-}
--- routes/web/web.js
+++ routes/web/web.js
@@ -1,19 +1,21 @@
 import { getRoutes } from 'fastify-vite/app'
-import { hydrateRoutes } from 'fastify-vite-vue/client.mjs'
+// import { hydrateRoutes } from 'fastify-vite-vue/client.mjs'
 
-const routes = [
-    {
-        path: '/',
-        name: 'home.index',
-        component: () => import("/resources/Pages/index.vue"),
-    },
-    {
-        path: '/hello',
-        name: 'home.hello',
-        component: () => import("/resources/Pages/hello.vue"),
-    }
-]
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'home.index',
+//         component: () => import("/resources/Pages/index.vue"),
+//     },
+//     {
+//         path: '/hello',
+//         name: 'home.hello',
+//         component: () => import("/resources/Pages/hello.vue"),
+//     }
+// ]
 
-export default import.meta.env.SSR
-    ? () => getRoutes(import.meta.globEager('/resources/Pages/*.vue'))
-    : () => getRoutes(hydrateRoutes(import.meta.glob('/resources/Pages/*.vue')))
+export default getRoutes(import.meta.globEager('/resources/Pages/*.vue'))
+
+// export default import.meta.env.SSR
+//     ? () => getRoutes(import.meta.globEager('/resources/Pages/*.vue'))
+//     : () => getRoutes(hydrateRoutes(import.meta.glob('/resources/Pages/*.vue')))
