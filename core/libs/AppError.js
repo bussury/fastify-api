0992ab974f8cec576f47167803ce4f76d04feef2
bussury <bussury.omary@gmail.com>
2022-05-20 07:34:30 +0300

    libs rule and request rule

---
+++ core/libs/AppError.js
@@ -0,0 +1,18 @@
+
+class AppError extends Error {
+    constructor (options) {
+        if (!options || !options.description) throw new Error('description param required')
+    
+        super()
+        this.description = options.description || undefined // default error description from errorCodes
+        this.message = options.message || this.description // message thrown by error
+        this.status = options.status || 500
+        this.code = options.code || 'UNEXPECTED_ERROR'
+        this.layer = options.layer || undefined
+        this.meta = options.meta || undefined
+        this.req = options.req || undefined
+        this.origin = options.origin || undefined // origin error data
+      }
+}
+
+export default {AppError}
---
+++ core/libs/RequestRule.js
@@ -0,0 +1,13 @@
+import { Assert as assert } from './Assert.js'
+
+class RequestRule {
+    constructor( schemaRule, {required = false, allowed = []} ={} ){
+        assert.instanceOf(schemaRule, Rule)
+        assert.object(arguments[1])
+        assert.boolean(required)
+        assert.array(allowed)
+
+        this.schemaRule = schemaRule
+        this.options = { required, allowed }
+    }
+}
