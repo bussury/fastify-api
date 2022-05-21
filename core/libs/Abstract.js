1e34f45c69d88a08d65ac8ba0a1837a2d6668254
bussury <bussury.omary@gmail.com>
2022-05-21 07:45:03 +0300

    cnofig

---
+++ core/libs/Abstract.js
@@ -0,0 +1,28 @@
+import { Assert as assert } from './Assert.js'
+
+class Abstract {
+    __abstractField (ctx, fieldName) {
+      assert.object(ctx, { required: true })
+      assert.string(fieldName, { notEmpty: true })
+  
+      if (!ctx[fieldName]) {
+        throw new Error(`${ctx.constructor.name}: ${fieldName} field not implemented`)
+      }
+    }
+  
+    __abstractMethod (ctx, methodName, argsQuantity) {
+      assert.object(ctx, { required: true })
+      assert.string(methodName, { notEmpty: true })
+      assert.integer(argsQuantity, { required: true, min: 1 })
+  
+      if (!ctx[methodName]) {
+        throw new Error(`${ctx.constructor.name}: ${methodName} method not implemented`)
+      }
+  
+      if (ctx[methodName].length !== argsQuantity) {
+        throw new Error(`${ctx.constructor.name}: ${methodName} method invalid arguments length`)
+      }
+    }
+  }
+  
+  export default { Abstract }
