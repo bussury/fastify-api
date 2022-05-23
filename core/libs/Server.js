08f786acc57d4a26f8646efb88d5c80b5d0e0719
bussury <bussury.omary@gmail.com>
2022-05-23 08:46:36 +0300

    pkg

--- core/libs/Server.js
+++ core/libs/Server.js
@@ -3,7 +3,7 @@ class Server{
 }
 
 function start({ port, host, controllers, middlewares, ErrorMiddleware, cookieSecret, reqLimit, logger }){
-    return new Promise (async ( resolve, reject) => {
+    return new Promise (async ( resolve, reject ) => {
 
     })
 }
