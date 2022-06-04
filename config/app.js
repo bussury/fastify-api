e0436df0f2c29a30431bb6958727ba0fa99fcacb
bussury <bussury.omary@gmail.com>
2022-06-04 23:40:03 +0300

    logger

--- config/app.js
+++ config/app.js
@@ -7,6 +7,13 @@ class AppConfig extends BaseConfig {
         this.port   =   this.set('APP_PORT', this.joi.number().port().required, 3000)
         this.host   =   this.set('APP_HOST', this.joi.string().hostname().required, 'localhost')
         this.name   =   this.set('APP_NAME', this.joi.string().required, 'fastify-api')
+        this.url    =   this.set('APP_URL', this.joi.string().required)
+    }
+
+    async init () {
+        //  we ca initilize any async function before
+
+
     }
 
 }
---
+++ core/lib/Logger.js
@@ -0,0 +1,116 @@
+import pino from "pino";
+
+import { Assert as assert } from "./Assert";
+import { AbstractLogger } from "./AbstractLogger";
+
+
+class Logger extends AbstractLogger {
+    constructor({appName, raw = false} = {}){
+        super()
+
+        assert.string(appName, { required: true })
+
+        this[$] = {
+      
+            fatalLogger: pino({
+              name: `${appName.toLowerCase()}::fatal`,
+              errorLikeObjectKeys: ['err', 'error'],
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            }),
+            errorLogger: pino({
+              name: `${appName.toLowerCase()}::error`,
+              errorLikeObjectKeys: ['err', 'error'],
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            }),
+            warnLogger: pino({
+              name: `${appName.toLowerCase()}::warn`,
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            }),
+            infoLogger: pino({
+              name: `${appName.toLowerCase()}::info`,
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            }),
+            debugLogger: pino({
+              level: 20,
+              name: `${appName.toLowerCase()}::debug`,
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            }),
+            traceLogger: pino({
+              level: 10,
+              name: `${appName.toLowerCase()}::trace`,
+              ...(!raw && { prettyPrint: { translateTime: 'SYS:standard' } })
+            })
+          }
+    }
+
+   /**
+   * ------------------------------
+   * @PRIVATE_HELPERS
+   * ------------------------------
+   */
+    fatal (message, error, meta) {
+        assert.string(message, { required: true })
+        assert.ok(error, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? { ...error, ...meta } : { ...error, meta }
+
+        this[$].fatalLogger.fatal(payload, message)
+      }
+    
+      error (message, error, meta) {
+        assert.string(message, { required: true })
+        assert.ok(error, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? { ...error, ...meta } : { ...error, meta }
+    
+        this[$].errorLogger.error(payload, message)
+      }
+    
+      warn (message, error, meta) {
+        assert.string(message, { required: true })
+        assert.ok(error, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? { ...error, ...meta } : { ...error, meta }
+    
+        this[$].warnLogger.warn(payload, message)
+      }
+    
+      /**
+       * ------------------------------
+       * @INFO_METHODS
+       * ------------------------------
+       */
+    
+      info (message, meta) {
+        assert.string(message, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? meta : { meta }
+    
+        this[$].infoLogger.info(payload, message)
+      }
+    
+      debug (message, meta) {
+        assert.string(message, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? meta : { meta }
+    
+        this[$].debugLogger.debug(payload, message)
+      }
+    
+      trace (message, meta) {
+        assert.string(message, { required: true })
+        assert.ok(meta)
+    
+        const payload = validator.isObject(meta) ? meta : { meta }
+    
+        this[$].traceLogger.trace(payload, message)
+      }
+
+}
+
+export {Logger}
