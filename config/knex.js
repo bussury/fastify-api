e96b518bd9577c2f03214bc56618206f95378f5a
bussury <bussury.omary@gmail.com>
2022-06-05 22:33:58 +0300

    knex setting

--- config/knex.js
+++ config/knex.js
@@ -4,12 +4,12 @@ import logger from "../logger.js";
 class KnexConfig extends BaseConfig{
     constructor(){
         super()
-        this.client =   'mysql',
+        this.client =   'mysql2',
         this.connection = {
             host: this.set('DB_HOST', this.joi.string().min(4).max(100).required(), 'localhost'),
             port: this.set('DB_PORT', this.joi.number().required(), '5432'),
             user: this.set('DB_USER', this.joi.string().min(4).max(100).required(), 'postgres'),
-            password: this.set('DB_PASSWORD', this.joi.string().valid('').required()),
+            password: this.set('DB_PASSWORD', this.joi.string().empty('').required()),
             database: this.set('DB_NAME', this.joi.string().min(4).max(100).required()),
             charset: this.set('DB_CHARSET', this.joi.string().valid('utf8').required(), 'utf8')
         }
