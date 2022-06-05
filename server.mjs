5a81d03c24fb7fe014f13eaa69dc110ea1004bfb
bussury <bussury.omary@gmail.com>
2022-06-05 22:33:39 +0300

    setting db

--- server.mjs
+++ server.mjs
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
+import knex from 'knex'
+import { Model } from 'objection'
 import { Server, assert } from './core/index.js'
-import { app, start }  from './config/index.js'
+import { app, start, knex as KnexConfig }  from './config/index.js'
 import logger from './logger.js'
 
 import bootstrap from './bootstrap/app.js'
@@ -16,7 +18,29 @@ start().then(() => {
   })
 }).then((serverParams) => {
   logger.info('Server initialized...', serverParams)
+  logger.debug('--- APP CONFIG ---')
+  logger.debug(`HOST: ${app.host}`)
+  logger.debug(`PORT: ${app.port}`)
+  logger.debug(`NAME: ${app.name}`)
+}).catch((error) =>{
+  stdout.write(error.stack)
+  logger.error('Server fails to initialize...', error)
 })
+  .then(() => { Model.knex(knex(KnexConfig)) })
+  .then(() => { 
+    logger.debug('Database initialized...')
+    logger.debug('--- SQL DATABASE CONFIG ---')
+    logger.debug(`CLIENT: ${KnexConfig.client}`)
+    logger.debug(`USER: ${KnexConfig.connection.user}`)
+    logger.debug(`HOST: ${KnexConfig.connection.host}`)
+    logger.debug(`PORT: ${KnexConfig.connection.port}`)
+    logger.debug(`DATABASE: ${KnexConfig.connection.database}`)
+  })
+  .then(() => {
+    logger.debug('---------')
+    logger.debug(`Server listened at ${app.host}:${app.port}`)
+    logger.debug('---------')
+   })
 
 // async function main() {
 //   const app = fastify({
