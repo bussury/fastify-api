931f2e54444eb73ded5c787c2c579043fb869f08
bussury <bussury.omary@gmail.com>
2022-10-18 11:20:53 +0300

    app:pkg

--- app/http/Controllers/BaseAction.js
+++ app/http/Controllers/BaseAction.js
@@ -35,6 +35,13 @@ class BaseAction {
     }
 
     static result ( result) {
+        assert.object(result, { notEmpty: true })
+        assert.boolean(result.success)
+        assert.integer(result.status)
+        assert.array(result.cookies)
+        assert.object(result.headers)
+        assert.string(result.message)
+        assert.ok(result.data)
         return {
             success: result.success || true,
             status: result.status || 200,
