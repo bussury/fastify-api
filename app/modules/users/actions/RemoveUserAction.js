e9867946a2c4ad48e0c0401a1e1102b693d29bc9
bussury <bussury.omary@gmail.com>
2022-12-03 10:34:54 +0300

    app:update

---
+++ app/modules/users/actions/RemoveUserAction.js
@@ -0,0 +1,23 @@
+import { BaseAction } from "../../../http/Controllers/BaseAction.js";
+import {UserDao} from '../../../models/UserDao.js'
+
+export default class RemoveUserAction extends BaseAction {
+    static get accessTag () {
+        return 'users:remove'
+    }
+    static async run (req){
+        const { currentUser } = req
+        const id = req.params.id
+
+        /**
+         * get use by the id and update his/her policy
+         */
+        
+        // @ TODO
+        // const model = await UserDao.baseGetById(id)
+        // await updateUserPolicy(model, currentUser)
+        await UserDao.remove(id)
+
+         return this.result({ message: `${id} was removed` })
+    }
+}
