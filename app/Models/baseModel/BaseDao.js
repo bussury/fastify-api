b060866e678e4d3159ab666ce0b3d82629313197
bussury <bussury.omary@gmail.com>
2023-10-26 11:57:47 +0300

    app;base Dao

--- app/Models/baseModel/BaseDao.js
+++ app/Models/baseModel/BaseDao.js
@@ -1,73 +1,79 @@
 import BaseModel from "./BaseModel.js";
-import { assert, errorCodes, AppError } from "../../../core/index.js"
+import { assert, errorCodes, AppError } from "../../../core/index.js";
 
-export  class BaseDao extends BaseModel {
-
-
-  static async create (data = {}) {
-    assert.object(data, { required: true })
+export class BaseDao extends BaseModel {
+  static async create(data = {}) {
+    assert.object(data, { required: true });
     // this.verifyUserId(data)
-      
-      const result =  await this.query().insert(data)
-      return result
-    
+
+    const result = await this.query().insert(data);
+    return result;
   }
 
-  static async update (id, data = {}) {
-    assert.id(id, { required: true })
-    assert.object(data, { required: true })
+  static async update(id, data = {}) {
+    assert.id(id, { required: true });
+    assert.object(data, { required: true });
 
-    const result = await this.query().patchAndFetchById(id, data)
+    const result = await this.query().patchAndFetchById(id, data);
 
-    return this.mapObject(result)
+    return this.mapObject(result);
   }
 
-
-  static async all ({ page, limit, filter, orderBy } = {}) {
-    let current_page = page - 1
-    assert.integer(Number(current_page), { required: true, message:"page number is required" })
-    assert.integer(Number(limit), { required: true, message:"limit number of obejcts is required" })
-    assert.object(filter, { required: false })
+  static async all({ page, limit, filter, orderBy } = {}) {
+    let current_page = page - 1;
+    assert.integer(Number(current_page), {
+      required: true,
+      message: "page number is required",
+    });
+    assert.integer(Number(limit), {
+      required: true,
+      message: "limit number of obejcts is required",
+    });
+    assert.object(filter, { required: false });
     // assert.id(filter.userId)
 
-    const offset = (current_page) * limit;
+    const offset = current_page * limit;
     const data = await this.query()
       .where({ ...filter })
       // .orderBy(orderBy.field, orderBy.direction)
       .limit(limit)
-      .offset(offset)
+      .offset(offset);
 
     // if (!data.results.length) return this.emptyPageResponse()
-    if (!data.length) return this.emptyPageResponse()
-    return data
+    if (!data.length) return this.emptyPageResponse();
+    return data;
   }
 
-  static async count (filter = {}) {
-    assert.object(filter, { required: true })
+  static async count(filter = {}) {
+    assert.object(filter, { required: true });
 
     const result = await this.query()
       .where({ ...filter })
-      .count('*')
-      .first()
-    if (!result.count) return 0
-    return Number(result.count)
+      .count("*")
+      .first();
+    if (!result.count) return 0;
+    return Number(result.count);
   }
 
-  static async getById (id) {
-    assert.id(id, { required: true })
-    const data = await this.query().findById(id).throwIfNotFound({message:`item with ${id} not found`})
-    if (!data) throw this.errorEmptyResponse()
-    return data
+  static async getById(id) {
+    assert.id(id, { required: true });
+    const data = await this.query()
+      .findById(id)
+      .throwIfNotFound({ message: `item with ${id} not found` });
+    if (!data) throw this.errorEmptyResponse();
+    return data;
   }
 
-  static remove (id) {
-    assert.id(id, { required: true })
-    return this.query().deleteById(id)
+  static remove(id) {
+    assert.id(id, { required: true });
+    return this.query().deleteById(id);
   }
 
-  static removeWhere (where = {}) {
-    assert.object(where, { required: true })
+  static removeWhere(where = {}) {
+    assert.object(where, { required: true });
 
-    return this.query().delete().where({ ...where })
+    return this.query()
+      .delete()
+      .where({ ...where });
   }
 }
--- app/Models/baseModel/BaseModel.js
+++ app/Models/baseModel/BaseModel.js
@@ -1,103 +1,109 @@
-import objection, { val } from "objection";
+import { Model } from "objection";
+// import objection, { val } from "objection";
 import BaseQuery from "./BaseQuery.js";
-import { assert, errorCodes, AppError } from "../../../core/index.js"
+import { assert, errorCodes, AppError } from "../../../core/index.js";
 
-const { NotFoundError, NotNullViolationError, ValidationError, UniqueViolationError} = objection
-class BaseModel extends objection.Model{
-    
-    constructor(){
-        super()
-    }
+const {
+  NotFoundError,
+  NotNullViolationError,
+  ValidationError,
+  UniqueViolationError,
+} = objection;
+class BaseModel extends Model {
+  constructor() {
+    super();
+  }
 
-    static get QueryBuilder(){
-        return BaseQuery
+  static get QueryBuilder() {
+    return BaseQuery;
+  }
+  static verifyUserId(data) {
+    assert.object(data, { required: true });
+
+    /**
+     * each entity that creates must have creator id (userId)
+     */
+    if (!data.email && !data.userId) {
+      throw new AppError({
+        ...errorCodes.UNPROCESSABLE_ENTITY,
+        message: "Please provide in action class 'userId' field",
+        layer: "DAO",
+      });
     }
-    static verifyUserId (data) {
-        assert.object(data, { required: true })
-    
-        /**
-         * each entity that creates must have creator id (userId)
-         */
-        if (!data.email && !data.userId) {
+  }
+
+  static query() {
+    return super.query.apply(this, arguments).onError((error) => {
+      return Promise.reject(error).catch((error) => {
+        error = error || error.nativeError;
+        if (error instanceof NotFoundError) {
           throw new AppError({
-            ...errorCodes.UNPROCESSABLE_ENTITY,
-            message: 'Please provide in action class \'userId\' field',
-            layer: 'DAO'
-          })
+            ...errorCodes.NOT_FOUND,
+            message: error.message,
+            layer: "DAO",
+          });
         }
-    }
-    
-    static query () {
-        return super.query.apply(this, arguments).onError(error => {
-          return Promise.reject(error)
-            .catch(error => {
-              error =  error ||error.nativeError
-              if (error instanceof NotFoundError) {
-                throw new AppError({
-                  ...errorCodes.NOT_FOUND,
-                  message: error.message,
-                  layer: 'DAO',
-                })
-              }
 
-              if (error instanceof UniqueViolationError) {
-                const constraint =  error.constraint
-                // const table = constraint.split('.')[0]
-                const column = constraint.split('.')[1].split('_')[1]
-              
-                throw new AppError({
-                  ...errorCodes.DB_DUPLICATE_CONFLICT,
-                  message: ` '${column}' duplicate / exists`,
-                  layer: 'DAO'
-                })
-              }
-    
-              if (error instanceof NotNullViolationError) {
-                throw new AppError({
-                  ...errorCodes.DB_NOTNULL_CONFLICT,
-                  message: `Not null conflict failed for table '${error.table}' and column '${error.column}'`,
-                  layer: 'DAO'
-                })
-              }
-     
-              throw new AppError({ ...errorCodes.DB, message: error.message, layer: 'DAO' })
-            })
-        })
-    }
-    
-       /**
-       * ------------------------------
-       * @HOOKS
-       * ------------------------------
-       */
-    
-        $beforeUpdate () {
-            this.updatedAt = new Date().toISOString()
-          }
+        if (error instanceof UniqueViolationError) {
+          const constraint = error.constraint;
+          // const table = constraint.split('.')[0]
+          const column = constraint.split(".")[1].split("_")[1];
 
+          throw new AppError({
+            ...errorCodes.DB_DUPLICATE_CONFLICT,
+            message: ` '${column}' duplicate / exists`,
+            layer: "DAO",
+          });
+        }
 
+        if (error instanceof NotNullViolationError) {
+          throw new AppError({
+            ...errorCodes.DB_NOTNULL_CONFLICT,
+            message: `Not null conflict failed for table '${error.table}' and column '${error.column}'`,
+            layer: "DAO",
+          });
+        }
 
- /**
+        throw new AppError({
+          ...errorCodes.DB,
+          message: error.message,
+          layer: "DAO",
+        });
+      });
+    });
+  }
+
+  /**
+   * ------------------------------
+   * @HOOKS
+   * ------------------------------
+   */
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+
+  /**
    * ------------------------------
    * @HELPERS
    * ------------------------------
    */
 
-  static errorEmptyResponse () {
-    return new AppError({ ...errorCodes.NOT_FOUND, layer: 'DAO' })
+  static errorEmptyResponse() {
+    return new AppError({ ...errorCodes.NOT_FOUND, layer: "DAO" });
   }
 
-  static emptyPageResponse () {
-    return { results: [], total: 0 }
+  static emptyPageResponse() {
+    return { results: [], total: 0 };
   }
 
-  static emptyListResponse () {
-    return []
+  static emptyListResponse() {
+    return [];
   }
 
-  static emptyObjectResponse () {
-    return {}
+  static emptyObjectResponse() {
+    return {};
   }
 }
 
-export default BaseModel
+export default BaseModel;
