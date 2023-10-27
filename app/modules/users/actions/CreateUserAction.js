f8c2a664f4bbc3774798f16574cef0414d3580d6
bussury <bussury.omary@gmail.com>
2023-10-27 08:28:47 +0300

    app

--- app/modules/users/actions/CreateUserAction.js
+++ app/modules/users/actions/CreateUserAction.js
@@ -1,17 +1,17 @@
 import { BaseAction } from "../../../http/Controllers/BaseAction.js";
-import {UserDao} from '../../../models/UserDao.js'
+import { UserDao } from "../../../Models/UserDao.js";
 
 export default class CreateUserAction extends BaseAction {
-    static get accessTag () {
-        return 'users:create'
-    }
-    static async run (ctx){
-        const user = await UserDao.create({
-            ...ctx.body
-        })
-        /**
-         * make email confirmation
-         */
-        return this.result({ data: user })
-    }
+  static get accessTag() {
+    return "users:create";
+  }
+  static async run(ctx) {
+    const user = await UserDao.create({
+      ...ctx.body,
+    });
+    /**
+     * make email confirmation
+     */
+    return this.result({ data: user });
+  }
 }
--- app/modules/users/actions/RemoveUserAction.js
+++ app/modules/users/actions/RemoveUserAction.js
@@ -1,22 +1,22 @@
 import { BaseAction } from "../../../http/Controllers/BaseAction.js";
-import {UserDao} from '../../../models/UserDao.js'
+import { UserDao } from "../../../Models/UserDao.js";
 
 export default class RemoveUserAction extends BaseAction {
-    static get accessTag () {
-        return 'users:remove'
-    }
-    static async run (req){
-        const { currentUser } = req
-        const id = req.params.id
+  static get accessTag() {
+    return "users:remove";
+  }
+  static async run(req) {
+    const { currentUser } = req;
+    const id = req.params.id;
 
-        /**
-         * get use by the id and update his/her policy
-         */
-        
-        // @ TODO
-        const user = await UserDao.getById(id)
-        // await updateUserPolicy(model, currentUser)
-         await UserDao.remove(id)
-         return this.result({ message: `user with an id ${id} was removed` })
-    }
+    /**
+     * get use by the id and update his/her policy
+     */
+
+    // @ TODO
+    const user = await UserDao.getById(id);
+    // await updateUserPolicy(model, currentUser)
+    await UserDao.remove(id);
+    return this.result({ message: `user with an id ${id} was removed` });
+  }
 }
