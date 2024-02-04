a8be2f5047a172d187b0d9e4a115cb609fb56609
bussury omary <bussury@bussurys-MacBook-Pro.local>
2024-02-04 19:19:43 +0300

    app:updte

--- database/config.js
+++ database/config.js
@@ -1,6 +1,14 @@
-import "dotenv/config";
+// import "dotenv/config";
 
-console.log(process.env.DB_HOST);
+import dotenv from "dotenv";
+import path from "node:path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
 const connection = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
